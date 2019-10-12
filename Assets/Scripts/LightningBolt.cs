6dd702af2162aaebf06f4d7bc044c3aea0e5adf3
daniel-ilett <danielilett@gmail.com>
2019-10-12 09:00:58 +0100

    Character movement working.
    Movement and collision are both functional. Work has started on the projectile.

---
+++ Assets/Scripts/LightningBolt.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LightningBolt : MonoBehaviour
+{
+    private new LineRenderer renderer;
+
+    private bool hasFired = false;
+
+    private List<Vector2> positions;
+    private Vector2 direction;
+
+    private void Start()
+    {
+        renderer = GetComponent<LineRenderer>();
+        Reset();
+    }
+
+    private void Update()
+    {
+        if(hasFired)
+        {
+            // Forward ray: 
+
+            // Raycast distance of deltaTime * speed in front.
+
+            // If hit enemy: hurt them.
+            // If hit player and cooldown is reached: get collected.
+            // If hit wall: reflect ray and do secondary raycast, 
+            //     ignoring that wall.
+
+            // Backward ray:
+
+            // Propogate backwards through position list and make
+            //     ray segments smaller. If ray segments disappear,
+            //     then go to next ray segment and delete.
+
+            RaycastHit2D[] hitObjects = 
+                Physics2D.RaycastAll(positions[0], direction, Time.deltaTime);
+            
+            foreach(var hitObject in hitObjects)
+            {
+                
+            }
+        }
+    }
+
+    public void Fire(Vector2 position, Vector2 direction)
+    {
+        hasFired = true;
+        this.direction = direction;
+    }
+
+    private void Reset()
+    {
+        gameObject.SetActive(false);
+        renderer.positionCount = 1;
+        positions = new List<Vector2>();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Vector2 moveVector;
+
+    private new Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CalculateMove();
+    }
+
+    private void CalculateMove()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        moveVector = new Vector2(horizontal, vertical);
+
+        if(moveVector.magnitude > 1.0f)
+        {
+            moveVector.Normalize();
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = moveVector * speed;
+    }
+}
