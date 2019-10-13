11deeb90866c4b49d9ea3b325aa20892af4837f1
daniel-ilett <danielilett@gmail.com>
2019-10-13 09:07:04 +0100

    Added spawning system.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyController enemyPrefab;
+
+    private void Start()
+    {
+        SpawnController.instance.AddSpawner(this);
+    }
+
+    public void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    private List<EnemySpawner> spawners = new List<EnemySpawner>();
+
+    [SerializeField]
+    private float frequency = 1.0f;
+
+    private float timer = 0.0f;
+
+    public static SpawnController instance = null;
+
+    // Register a singleton.
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > frequency)
+        {
+            // Tell a spawner to create an enemy.
+            int rand = Random.Range(0, spawners.Count);
+            spawners[rand].SpawnEnemy();
+            timer = 0.0f;
+        }
+    }
+
+    public void AddSpawner(EnemySpawner newSpawner)
+    {
+        spawners.Add(newSpawner);
+    }
+}
