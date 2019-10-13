56ba7a0c76fe8805628260dcd87a41632abc0314
daniel-ilett <danielilett@gmail.com>
2019-10-13 12:00:40 +0100

    Improved camera controller.
    The camera zooms out to make sure both the player and lightning bolt are visible.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,12 +9,24 @@ public class CameraController : MonoBehaviour
 
     [SerializeField]
     private float speed = 1.0f;
+
+    [SerializeField]
+    private float minSize = 4.0f;
     
     private LightningBolt lightningBolt;
 
     private Vector3 targetPos = Vector3.zero;
+    private float targetZoom = 4.0f;
 
-    // Cache components and set initial target.
+    private new Camera camera;
+
+    // Cache components.
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Cache otjher gameObjects and set initial target.
     private void Start()
     {
         player = PlayerController.instance;
@@ -26,12 +38,14 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         SetTargetPos();
+        SetCameraZoom();
     }
 
     // Move towards the target position.
     private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.fixedDeltaTime * speed);
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, targetZoom, Time.fixedDeltaTime * speed);
     }
 
     // Set an anchor point between the player and head of the laser.
@@ -45,6 +59,25 @@ public class CameraController : MonoBehaviour
         else
         {
             SetTargetPos(player.transform.position);
+        }
+    }
+
+    // Zoom in and out depending on the bolt and player positions.
+    private void SetCameraZoom()
+    {
+        if(lightningBolt.isActiveAndEnabled)
+        {
+            float xDist = Mathf.Abs(player.transform.position.x - lightningBolt.GetHeadPosition().x);
+            float yDist = Mathf.Abs(player.transform.position.y - lightningBolt.GetHeadPosition().y);
+
+            float xZoom = xDist / camera.aspect * 0.75f;
+            float yZoom = yDist * 0.75f;
+
+            targetZoom = Mathf.Max(Mathf.Max(xZoom, yZoom), minSize);
+        }
+        else
+        {
+            targetZoom = minSize;
         }
     }
 
