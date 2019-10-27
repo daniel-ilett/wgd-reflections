8ed6f7c16210906e8a95f20867d9d67d3a1170ef
Daniel Ilett <danielilett@gmail.com>
2019-10-27 22:56:01 +0000

    Added background for Start and Game Over screens.

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("FireKeyboard") || Input.GetButtonDown("FireController"))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
